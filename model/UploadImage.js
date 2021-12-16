bd47efeebe4bfc8d40ab798b9fe88e2050d801bf
firestormDev <79257881+firestormDev@users.noreply.github.com>
2021-12-16 16:17:46 -0300

    Add files via upload

--- model/UploadImage.js
+++ model/UploadImage.js
@@ -1,58 +1,32 @@
-const multer = require('multer');
-
-module.exports = (multer({
-    storage: multer.diskStorage({
-        destination:(req,res,cd) =>{
-            cd(null, './public/upload/users')
-        },
-
-        filename:(req,file,cb) => {
-            cd(null, Date.now().toString() + "_" +file.originalname) 
-        }
-        }),
-    fileFilter:(req,file,cb) => {
-        const extensaoImg = ['image/png', 'image/jpg', 'image/jpeg'].find(formatoAceito => formatoAceito == file.mimetype);
-
-        if(extensaoImg){
-            return cb(null,true);
-        }
-
-        return cb(null,false);
-    }
-
-}));
-
-//const MongoClient = require('mongodb').MongoClient;
+const multer = require("multer");
 
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://protomania:tinti8607@leaflix-east.dvc2t.mongodb.net/leaflix-east?retryWrites=true&w=majority";
-//const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
   client.close();
 });
 
+class UploadImage {
 
-module.exports = class UploadImage {
-    static async find(busca) {        
-       // const conn = await MongoClient.connect('mongodb://127.0.0.1:27017/netflixdb'),
+       async find(busca) {        
+       //const conn = await MongoClient.connect('mongodb://127.0.0.1:27017/netflixdb'),
        const conn = await MongoClient.connect('mongodb+srv://protomania:tinti8607@leaflix-east.dvc2t.mongodb.net/leaflix-east?retryWrites=true&w=majority'),
 
-                db = conn.db();  
+            db = conn.db();  
                 
             if(busca)
                 return await db.collection('postsImagens')
                 .find({ image : new RegExp('^' + busca)})
                 .toArray();
             
-                
         return await db.collection('postsImagens').find().toArray();
 
     }
 
-    static async insert (image){
+    async insert (image){
+
         //const conn = await MongoClient.connect('mongodb://127.0.0.1:27017/netflixdb'),
         const conn = await MongoClient.connect('mongodb+srv://protomania:tinti8607@leaflix-east.dvc2t.mongodb.net/leaflix-east?retryWrites=true&w=majority'),
         db = conn.db();  
@@ -60,6 +34,47 @@ module.exports = class UploadImage {
         console.log(image);
 
     }
+
+    //uploadFile
+    async image(req, res, next) {
+
+        const storage = multer.diskStorage({
+            destination: function (req, file, cb) {
+                cb(null, "public/upload/users/");
+            },
+            filename: function (req, file, cb) {
+                cb(null, file.originalname);
+            }
+        });
+        const upload = multer({
+            storage,
+            fileFilter: function (req, file, cb) {
+                const allowedMimes = [
+                    'image/jpeg',
+                    'image/jpg',
+                    'image/png',
+                    'image/gif',
+                    'video/mp4'
+                ];
+                if (allowedMimes.includes(file.mimetype)) {
+                    cb(null, true);
+                } else {
+                    cb('O formato do arquivo é inválido! Arquivos permitidos: jpeg, jpg, png, gif e mp4');
+                }
+            }
+        }).fields([{ name: 'image', maxCount: 1 }]);
+
+
+        upload(req, res, function (err) {
+            if (err instanceof multer.MulterError) {
+                req.flash('info', err);
+            } else if (err) {
+                req.flash('info', err);
+            }
+            next()
+        })
+
+    } //uploadFile
+
 }
-
-
+module.exports = new UploadImage();
